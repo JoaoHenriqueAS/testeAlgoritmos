7204a8d8cb7259219f6235947111a9223600a9e6
João Henrique <Joao@Mac-mini-de-ahazou.local>
2021-03-17 21:35:43 -0300

    Criação do algoritmo para identificar palindromos

--- src/palindromo/palindromo.java
+++ src/palindromo/palindromo.java
@@ -1,10 +1,19 @@
 package palindromo;
 
+import javax.swing.JOptionPane;
+
 public class palindromo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String frase = JOptionPane.showInputDialog(null,"Digite uma frase: ");
+		frase = frase.replace(" ", "").toLowerCase();
+		String fraseInvertida = new StringBuilder(frase).reverse().toString();
+				
+		if(frase.equals(fraseInvertida)) {
+			System.out.println(1);
+		} else System.out.println(0);
+		
 	}
 
 }
