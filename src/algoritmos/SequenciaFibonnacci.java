4c047dcad8a187d7b2f9ee931eb6506487065589
João Henrique <Joao@Mac-mini-de-ahazou.local>
2021-03-17 23:05:42 -0300

    Algoritmo para exibir sequencia de Fibonnacci

---
+++ src/algoritmos/SequenciaFibonnacci.java
@@ -0,0 +1,26 @@
+package algoritmos;
+
+import java.util.Scanner;
+
+public class SequenciaFibonnacci {
+	
+	public static void main(String[] args) {
+		
+		int n1 = 1;
+		int n2 = 1;
+		int nf;
+		Scanner valor = new Scanner(System.in);
+		System.out.println("Digite um numero: ");
+		int n = valor.nextInt();
+		
+		System.out.print(n1 + "," + n2);
+		for(int count = 3; count <= n; count++) {
+			nf = n1 + n2;
+			System.out.print("," + nf);
+			n1 = n2;
+			n2 = nf;
+		}
+	}
+
+
+}
